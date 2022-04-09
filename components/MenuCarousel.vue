67596b4cfda479996cea5f6e20d5cd7ed8257739
DomagojFL <domagoj.freelance.hr@gmail.com>
2022-04-10 00:52:00 +0200

    update

--- components/MenuCarousel.vue
+++ components/MenuCarousel.vue
@@ -11,7 +11,7 @@
         :clickable="false"
         :border="0"
         :width="600"
-        :height="820"
+        :height="876"
       >
         <slide v-for="(slide, i) in slide" :index="i" :key="i">
           <template slot-scope="{ index, isCurrent, leftIndex, rightIndex }">
