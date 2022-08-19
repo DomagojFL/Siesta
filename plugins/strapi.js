ccb28c22ad1420dc53a4f2da60f7edb09e9f5cd0
Domagoj Vrkic <domagojfrontend@gmail.com>
2022-08-20 00:14:52 +0200

    maybe update

--- plugins/strapi.js
+++ plugins/strapi.js
@@ -2,7 +2,7 @@ import Strapi from "strapi-sdk-js";
 
 export default ({ app }, inject) => {
   const strapi = new Strapi({
-    url: "https://siesta-backend.herokuapp.com/",
+    url: "https://siesta-backend.herokuapp.com",
     prefix: "/api",
     store: {
       key: "strapi_jwt",
